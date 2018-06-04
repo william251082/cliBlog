5828f2156013173382a23175aec2bafa0d07c8eb
William del Rosario <williamdelrosario@yahoo.com>
2018-06-04 19:03:06 +0200

    Initial Commit

---
+++ app.py
@@ -0,0 +1,13 @@
+__author__ = 'william'
+
+import pymongo
+
+uri = "mongodb://127.0.0.1:27017"
+client = pymongo.MongoClient(uri)
+database = client['fullstack']
+collection = database['students']
+
+students = collection.find({})
+
+for student in students:
+    print(students)
