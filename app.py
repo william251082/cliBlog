d47c8113eba7f768172541e015e2c96b96a3981b
William del Rosario <williamdelrosario@yahoo.com>
2018-06-05 10:50:33 +0200

    @classmethods and blog class

--- app.py
+++ app.py
@@ -4,9 +4,19 @@ from models.post import Post
 
 Database.initialize()
 
-post = Post("Post1 title", "Post1 content", "Post1 author")
-post2 = Post("Post2 title", "Post2 content", "Post2 author")
-post2.content = "Some different content"
+blog = Blog(author="Jose",
+            title="Another great post",
+            description="Sample description"
+            )
 
-print(post.content)
-print(post2.content)
+blog.new_post()
+blog.save_to_mongo()
+Blog.from_mongo()
+blog.get_posts()
+
+# post = Post("Post1 title", "Post1 content", "Post1 author")
+# post2 = Post("Post2 title", "Post2 content", "Post2 author")
+# post2.content = "Some different content"
+
+# print(post.content)
+# print(post2.content)
