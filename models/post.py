d4b46c45d118b4935fef941c08e62e1991cb646c
William del Rosario <williamdelrosario@yahoo.com>
2018-06-05 09:15:59 +0200

    Python datetime

--- models/post.py
+++ models/post.py
@@ -1,11 +1,12 @@
 import uuid
 
 from database import Database
+import datetime
 
 
 class Post(object):
 
-    def __init__(self, blog_id, title, content, author, date, id=None):
+    def __init__(self, blog_id, title, content, author, date=datetime.datetime.utcnow(), id=None):
         self.blog_id = blog_id
         self.title = title
         self.content = content
